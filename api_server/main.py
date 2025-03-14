039cbbbd8fc92268c265619d85b145146326f3e2
tak-w <iamtakuhisam@gmail.com>
2025-03-13 20:09:45 -0400

    modify allow_origins

--- api_server/main.py
+++ api_server/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # Configure CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://takumanken.github.io"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
