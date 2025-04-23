d0f57b6695679cdef4c8e25d2bb21c3ee4a58465
tak-w <iamtakuhisam@gmail.com>
2025-04-22 20:02:09 -0400

    refactor: simplify nta_population view by reducing selected columns

--- application/backend/sqls/nta_population.sql
+++ application/backend/sqls/nta_population.sql
@@ -1,13 +1,8 @@
 CREATE OR REPLACE VIEW nta_population AS
 
 SELECT
-  "NTA Name" as nta_name,
-  "Pop 20" as population_2020,
-  "Nta2020" as neighborhood_code,
-  "Boro Code" as boro_code,
-  UPPER("Boro Name") as borough,
-  "NTA Abbrev" as nta_abbrev,
-  "NTA Type" as nta_type,
-  "Cdta2020" as cdta_2020,
-  "CDTA Name" as cdta_name
+  nta2020 as neighborhood_code,
+  name,
+  borough,
+  population_2020
 FROM read_csv('data/nta_population.csv');
